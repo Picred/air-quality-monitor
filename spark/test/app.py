b95af4686ea0763e1742b3054a30aae205efc10f
Andrei Stefan <102211437+Picred@users.noreply.github.com>
2024-05-23 21:10:51 +0000

    chore: test spark

--- spark/test/app.py
+++ spark/test/app.py
@@ -1,34 +1,28 @@
+from __future__ import print_function
+
+import sys
+
+from pyspark import SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, FloatType, StringType
-import folium
+from pyspark.streaming import StreamingContext
+from pyspark.sql.dataframe import DataFrame
 
-spark = SparkSession.builder \
-    .appName("Spark Geo Map") \
-    .getOrCreate()
+def elaborate(batch_df: DataFrame, batch_id: int):
+  batch_df.show(truncate=False)
 
-schema = StructType([
-    StructField("latitude", FloatType(), nullable=False),
-    StructField("longitude", FloatType(), nullable=False),
-    StructField("info", StringType(), nullable=True)
-])
+sc = SparkContext(appName="PythonStructuredStreamsKafka")
+spark = SparkSession(sc)
+sc.setLogLevel("WARN")
 
-data = [
-    (41.8781, -87.6298, "Chicago, USA - Informazioni XYZ"),  
-    (51.5074, -0.1278, "London, UK - Informazioni ABC"),   
-    (35.6895, 139.6917, "Tokyo, Japan - Altre informazioni"),  
-    (-33.8688, 151.2093, "Sydney, Australia - Altre informazioni"), 
-    (40.7128, -74.0060, "New York City, USA - Altre informazioni")
-]
+kafkaServer="kafkaServer:9092"
+topic = "tap"
 
-df = spark.createDataFrame(data, schema)
+# Streaming Query
 
-latitudes = df.select("latitude").rdd.flatMap(lambda x: x).collect()
-longitudes = df.select("longitude").rdd.flatMap(lambda x: x).collect()
-infos = df.select("info").rdd.flatMap(lambda x: x).collect()
-
-mymap = folium.Map(location=[latitudes[0], longitudes[0]], zoom_start=2)
-
-for lat, lon, info in zip(latitudes, longitudes, infos):
-    folium.Marker([lat, lon], popup=folium.Popup(info, parse_html=True)).add_to(mymap)
-
-mymap.save("mymap.html")
+df = spark \
+  .read \
+  .format("kafka") \
+  .option("kafka.bootstrap.servers", "host1:port1,host2:port2") \
+  .option("subscribe", "topic1") \
+  .load()
+df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
