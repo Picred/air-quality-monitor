f16af23cc75bdb5e74c03574a0c4cbd93e9519e4
Andrei Stefan <102211437+Picred@users.noreply.github.com>
2024-06-05 22:28:17 +0000

    feat: spark to Kibana working

--- spark/code/app.py
+++ spark/code/app.py
@@ -2,9 +2,14 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, from_json, to_timestamp
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType, TimestampType
 from pyspark.ml.feature import VectorAssembler, StringIndexer
-from pyspark.ml.regression import RandomForestRegressor
+from pyspark.ml.regression import LinearRegression
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml import Pipeline
+import logging
+
+# Configurazione del logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Configurazione delle variabili
 kafkaServer = "kafkaServer:9092"
@@ -14,24 +19,14 @@ es_host = "elasticsearch:9200"
 
 # Creazione della sessione Spark
 spark = SparkSession.builder \
-    .appName("AirQualityPrediction") \
+    .appName("AirQualityMonitor") \
     .config("spark.sql.streaming.checkpointLocation", "/tmp/checkpoints") \
     .getOrCreate()
-
-# Test di connessione a Elasticsearch
-test_df = spark.createDataFrame([("test",)], ["message"])
-test_df.write \
-    .format("org.elasticsearch.spark.sql") \
-    .option("es.resource", f"{elastic_index}") \
-    .option("es.nodes", es_host) \
-    .option("es.nodes.wan.only", "true") \
-    .mode("append") \
-    .save()
 
 # Definizione dello schema per i dati in input
 schema = StructType([
     StructField("wind_direction", DoubleType(), True),
-    StructField("weather_timestamp", StringType(), True),
+    StructField("weather_timestamp", StringType(), True), # TimestampType()
     StructField("state", StringType(), True),
     StructField("city", StringType(), True),
     StructField("gps_lat", DoubleType(), True),
@@ -43,13 +38,11 @@ schema = StructType([
     StructField("aqicn", IntegerType(), True),
     StructField("icon", StringType(), True),
     StructField("gps_lon", DoubleType(), True),
-    StructField("@timestamp", StringType(), True),
     StructField("maincn", StringType(), True),
     StructField("pression", DoubleType(), True),
-    StructField("pollution_timestamp", StringType(), True),
+    StructField("pollution_timestamp", StringType(), True), # TimestampType()
     StructField("aqius", IntegerType(), True),
     StructField("humidity", IntegerType(), True),
-    StructField("@version", StringType(), True)
 ])
 
 # Lettura dei dati da Kafka
@@ -69,19 +62,24 @@ df = df.withColumn("weather_timestamp", to_timestamp(col("weather_timestamp")))
 df = df.withColumn("pollution_timestamp", to_timestamp(col("pollution_timestamp")))
 
 # Codifica delle variabili categoriche
-indexers = [StringIndexer(inputCol=column, outputCol=column+"_index") for column in ["state", "city", "tags", "country"]]
+indexers = [StringIndexer(inputCol=column, outputCol=column+"_index") for column in ["state", "city", "country"]]
 pipeline = Pipeline(stages=indexers)
 
 # Selezione delle caratteristiche
-feature_columns = ["wind_direction", "wind_speed", "temperature", "pression", "humidity", "state_index", "city_index", "tags_index", "country_index"]
+feature_columns = ["wind_direction", "wind_speed", "temperature", "pression", "humidity", "state_index", "city_index", "country_index"]
 assembler = VectorAssembler(inputCols=feature_columns, outputCol="features")
 
 # Modello di regressione
-rf = RandomForestRegressor(featuresCol="features", labelCol="aqius", maxBins=12)
+lr = LinearRegression(featuresCol="features", labelCol="aqius")
 
 # Funzione di aggiornamento del modello
 def train_and_predict(batch_df, batch_id):
     if not batch_df.isEmpty():
+        logger.info("Processing batch id: %d", batch_id)
+        logger.info("Batch data schema:")
+        batch_df.printSchema()
+        logger.info("Batch data preview:")
+        batch_df.show(truncate=False)
         try:
             # Preprocessing dei dati nel batch
             batch_df = pipeline.fit(batch_df).transform(batch_df)
@@ -91,14 +89,13 @@ def train_and_predict(batch_df, batch_id):
             (trainingData, testData) = batch_df.randomSplit([0.8, 0.2])
 
             # Addestramento del modello
-            model = rf.fit(trainingData)
+            model = lr.fit(trainingData)
 
             # Predizione e valutazione
             predictions = model.transform(testData)
             evaluator = RegressionEvaluator(labelCol="aqius", predictionCol="prediction", metricName="rmse")
             rmse = evaluator.evaluate(predictions)
-            print(f"Root Mean Squared Error (RMSE) on test data = {rmse}")
-
+            logger.info(f"Root Mean Squared Error (RMSE) on test data = {rmse}")
             # Esegui previsioni sui dati in streaming
             streaming_predictions = model.transform(batch_df)
             
@@ -110,7 +107,11 @@ def train_and_predict(batch_df, batch_id):
             )
             
             # Log dei dati prima di scriverli su Elasticsearch
-            output.show()
+            print("Data before writing to Elasticsearch:")
+            output.show(truncate=False)
+
+            # Controlla lo schema del DataFrame output
+            output.printSchema()
 
             # Scrittura dei dati su Elasticsearch
             output.write \
@@ -121,10 +122,10 @@ def train_and_predict(batch_df, batch_id):
                 .mode("append") \
                 .save()
         except Exception as e:
-            print("An IllegalArgumentException occurred:", str(e))
-            # Gestisci l'eccezione in base alle tue esigenze, ad esempio ignorando il batch o
-            # modificando dinamicamente il valore di maxBins e riprovando ad addestrare il modello
-            # ...
+            print("An error occurred:", str(e))
+            logger.error("An error occurred during batch processing: %s", e)
+    else:
+        logger.info("No data received in batch id: %d", batch_id)
 
 # Esecuzione del modello di machine learning sui dati in streaming
 query = df.writeStream \
